4be855652e32da4401a26290cd09f1233078c358
Saurav Singh <58138324+SauravSinghPaliwal@users.noreply.github.com>
2025-04-14 18:50:33 +0530

    xsc

--- src/app/lib/blog.js
+++ src/app/lib/blog.js
@@ -1,29 +1,29 @@
-import matter from "gray-matter";
+// import matter from "gray-matter";
 
-// Manually import markdown files
-import post1 from '../../posts/llm-tutorial.mdx?raw';
+// // Manually import markdown files
+// import post1 from '../../posts/llm-tutorial.mdx?raw';
 
-// You must use `?raw` to import the file as a string
-const allPosts = [
-  { slug: "post1", content: post1 },
-];
+// // You must use `?raw` to import the file as a string
+// const allPosts = [
+//   { slug: "post1", content: post1 },
+// ];
 
-export function getSortedPostsData() {
-  return allPosts.map(({ slug, content }) => {
-    const { data } = matter(content);
-    return {
-      slug,
-      ...data,
-    };
-  }).sort((a, b) => new Date(b.date) - new Date(a.date));
-}
+// export function getSortedPostsData() {
+//   return allPosts.map(({ slug, content }) => {
+//     const { data } = matter(content);
+//     return {
+//       slug,
+//       ...data,
+//     };
+//   }).sort((a, b) => new Date(b.date) - new Date(a.date));
+// }
 
-export function getPostBySlug(slug) {
-  const post = allPosts.find((p) => p.slug === slug);
-  const { data, content } = matter(post.content);
-  return {
-    slug,
-    ...data,
-    content,
-  };
-}
+// export function getPostBySlug(slug) {
+//   const post = allPosts.find((p) => p.slug === slug);
+//   const { data, content } = matter(post.content);
+//   return {
+//     slug,
+//     ...data,
+//     content,
+//   };
+// }
